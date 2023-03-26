d03096452619e11700c963fd170e6d7bb2affeaa
Rosen Toshev <rtoscheff@gmail.com>
2023-03-26 03:44:54 +0100

    Section 6 until sever tests 1

---
+++ src/test/server_app/utils/Utils.test.ts
@@ -0,0 +1,56 @@
+import { IncomingMessage } from "http";
+import { getRequestBody } from "../../../app/server_app/utils/Utils";
+const requestMock = {
+  on: jest.fn(),
+};
+
+const someObject = {
+  name: "John",
+  age: 30,
+  city: "Paris",
+};
+
+const someObjectAsString = JSON.stringify(someObject);
+
+describe("getRequestBody test suite", () => {
+  it("should return object for valid JSON", async () => {
+    requestMock.on.mockImplementation((event, cb) => {
+      if (event == "data") {
+        cb(someObjectAsString);
+      } else {
+        cb();
+      }
+    });
+
+    const actual = await getRequestBody(requestMock as any as IncomingMessage);
+
+    expect(actual).toEqual(someObject);
+  });
+
+  it("should throw error for invalid JSON", async () => {
+    requestMock.on.mockImplementation((event, cb) => {
+      if (event == "data") {
+        cb("a" + someObjectAsString);
+      } else {
+        cb();
+      }
+    });
+
+    await expect(getRequestBody(requestMock as any)).rejects.toThrow(
+      'Unexpected token \'a\', "a{"name":""... is not valid JSON'
+    );
+  });
+
+  it("should throw error for unexpected error", async () => {
+    const someError = new Error("Something went wrong!");
+    requestMock.on.mockImplementation((event, cb) => {
+      if (event == "error") {
+        cb(someError);
+      }
+    });
+
+    await expect(getRequestBody(requestMock as any)).rejects.toThrow(
+      someError.message
+    );
+  });
+});
